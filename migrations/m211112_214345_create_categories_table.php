a122d062e269e17c3e3afdbd5599780a769f7f39
Konstantin Naumov <naumov-web@yandex.ru>
2021-11-13 00:57:05 +0300

    Create application and docker compose

---
+++ migrations/m211112_214345_create_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table "categories"
+ */
+class m211112_214345_create_categories_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('categories', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(100),
+            'parent_id' => $this->integer()->unsigned()->null(),
+            'level' => $this->integer()->unsigned()
+        ]);
+
+        $this->addForeignKey(
+            'fk-categories-parent_id',
+            'categories',
+            'parent_id',
+            'categories',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('categories');
+    }
+}
---
+++ views/site/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Simple Yii2 CRUD example';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Simple Yii2 CRUD example</h1>
+
+        <p class="lead">You can create, update, view and delete simple entity and get entities via API.</p>
+    </div>
+</div>
